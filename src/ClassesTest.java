359a5afbca7cf9da529b720a74f3a9a70fe91e67
Tarinaky <Tarinaky@gmail.com>
2012-02-25 08:15:50 +0000

    Added an assert to ClassesTest.testAddLink that verifies that both classes contain the same link.

--- src/ClassesTest.java
+++ src/ClassesTest.java
@@ -5,18 +5,19 @@ public class ClassesTest extends TestCase {
 	private Classes testClasses;
 
 	public void setUp() {
-		testClasses = new Classes("test");
+		testClasses = new Classes("testClasses");
 	}
 
 	public void testAddLink() {
 		//Add a link to the class, retrieve it and remove it.
 		Classes classB = new Classes("classB");
 		Link link = new Link("link");
-		testClasses.addLink(link);
-		classB.addLink(link);
-
-		testClasses.getLink("TestLink-1");
-		testClasses.removeLink("TestLink-1");
+		
+		link.setClassA(testClasses);
+		link.setClassB(testClasses);
+		
+		//Assert that both classes point to the same link.
+		assertTrue(testClasses.getLink("link") == classB.getLink("link") );
 	}
 
 	public void testComments() {
