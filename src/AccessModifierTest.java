385fd7153f9e5df7e9901e81ba657e24e442a7eb
Tarinaky <tarinaky@gmail.com>
2012-02-25 05:59:13 +0000

    Added tests to AccessModifier and fixed bug in run_tests.

---
+++ src/AccessModifierTest.java
@@ -0,0 +1,17 @@
+import junit.framework.*;
+import junit.framework.Assert.*;
+
+public class AccessModifierTest extends TestCase {
+	private AccessModifier testAccessModifier;
+
+	public void setUp() {
+	}
+
+	public void testSetGet() {
+		testAccessModifier = new AccessModifier(AccessModifier.PUBLIC);
+		assertTrue(testAccessModifier.mode == AccessModifier.PUBLIC);
+
+		testAccessModifier = new AccessModifier(AccessModifier.PRIVATE);
+		assertTrue(testAccessModifier.mode == AccessModifier.PRIVATE);
+	}
+}
--- src/run_tests.sh
+++ src/run_tests.sh
@@ -4,10 +4,16 @@ error=0
 
 for file in *Test.java
 do
+	compileError=0
 	echo " * Compiling ${file}."
-	javac -cp .:/usr/lib/junit.jar ${file} || error=1 && continue
-	echo " * Running tests, ${file}."
-	java -cp .:/usr/lib/junit.jar junit.textui.TestRunner ${file/.java/} || error=1
+	javac -cp .:/usr/lib/junit.jar ${file} || compileError=1
+	if [ $compileError -eq 0 ]
+	then
+		echo " * Running tests, ${file}."
+		java -cp .:/usr/lib/junit.jar junit.textui.TestRunner ${file/.java/} || error=1
+	else
+		error=1
+	fi
 done
 
 if [ $error -eq 1 ]
