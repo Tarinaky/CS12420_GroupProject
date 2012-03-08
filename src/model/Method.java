8c7a9f463be43a4584f8fc36984c701f6c73e0f0
Aidan <fewstera@gmail.com>
2012-03-08 06:41:54 +0000

    Changed parameters from List to TreeMap

--- src/model/Method.java
+++ src/model/Method.java
@@ -1,26 +1,35 @@
 package model;
 import java.util.*;
 
+import apple.laf.JRSUIUtils.Tree;
+
 public class Method extends Field implements java.io.Serializable {
 	/**
 	 * This class is the special case of a field that is a method.
 	 * Method's return type is stored as the type of field (since it expands to the same type during execution). This leaves the parameters/arguments list as the only real difference.
 	 */
-	private List<Field> parameters;
+	private Map<String, Field> parameters;
 
 	public Method(String label) {
 		/**
 		 * Ctor
 		 */
 		super(label);
-		parameters = new LinkedList<Field>();
+		parameters = new TreeMap<String,Field>();
 	}
 
-	public List<Field> getParameters() {
+	public Collection<Field> getParameters() {
 		/**
 		 * Return a handle to the parameters list. This does not return a copy but instead returns the actual object. Alterations can be performed 'in place' by adding or removing from the handle returned by this method.
 		 */
-		return parameters;
+		return parameters.values();
+	}
+	
+	public Field getParameter(String label) {
+		/**
+		 * Retrieve a field by its label. Returns null if no such field exists.
+		 */
+		return parameters.get(label);
 	}
 	
 	public void addParameter(Field parameter)
@@ -28,7 +37,7 @@ public class Method extends Field implements java.io.Serializable {
 		/**
 		 * Add a single parameter to this method's current parameter list.
 		 */
-		parameters.add(parameter);
+		parameters.put(parameter.getLabel(), parameter);
 	}
 
 }
